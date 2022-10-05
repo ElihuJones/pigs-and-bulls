a5045e8911a366936425d4bdb2ad143500c529b2
craigkelleher <kelleher.craig@gmail.com>
2022-10-05 15:08:32 -0700

    Decomposition of main project

--- src/main/java/PigsBulls.java
+++ src/main/java/PigsBulls.java
@@ -5,78 +5,147 @@ import java.io.Reader;
 
 public class PigsBulls {
 
+  static final int NUMBER_OF_CHARACTERS = 5;
+  private final Reader reader = new InputStreamReader(System.in);
+  private final BufferedReader buffer = new BufferedReader(reader);
+  private final UserInputValidator validateUserInput = new UserInputValidator();
+  private final OutputMessages messages;
   public static int turnCounter;
-  public static String secret;
-
+  private String userString;
+  private String secret;
+  private int bulls;
+  private int pigs;
 
   public static void main(String[] args) {
-    try {
-      countBullsAndPigs();
-    } catch (IOException e) {
-      System.out.println("Can't read Input, Game Over!");
-    }
+
+    boolean flag;
+    OutputMessages messages = new OutputMessages();
+    PigsBulls pigsBulls = new PigsBulls(messages);
+
+    messages.displayLogo();
+    messages.displayInstructions();
+    messages.displayMessageProvideFiveCharacters();
+
+    do {
+      flag = pigsBulls.checkCharacters();
+    } while (!flag);
   }
 
-  public static void countBullsAndPigs() throws IOException {
-    Reader reader = new InputStreamReader(System.in);
-    BufferedReader buffer = new BufferedReader(reader);
-    String secret = WordList.generateRandomWord().toString();
-    int bulls = 0;
-    int pigs = 0;
-    System.out.println("Welcome to the Pigs and Bulls game!");
-    while (turnCounter < 5) {
-      String guess = null;
-      do {
-        System.out.println("Please enter a 5 letter word");
-        try {
-          guess = buffer.readLine().toUpperCase().trim();
-          if (guess.matches("^[A-Z]{5}$")) {
-            break;
-          } else {
-            throw new IllegalArgumentException();
-          }
-        } catch (IllegalArgumentException e) {
-          System.out.println("Invalid Input!");
-        }
-      } while (true);
-      for (int i = 0; i < guess.length(); i++) {
-        if (guess.charAt(i) == secret.charAt(i)) {
-          bulls++;
-        }
+  PigsBulls(OutputMessages messages) {
+    this.messages = messages;
+    //getSecret();
+  }
+
+  boolean checkCharacters() {
+    if (verifyOnlyCharInput()) {
+      return checkMatches();
+    }
+    return false;
+  }
+
+  private boolean checkMatches() {
+    countBullsAndPigs();
+    return getResult();
+  }
+
+  void countBullsAndPigs() {
+/*    for (int i = 0; i < userString.length(); i++) {
+      if (userString.charAt(i) == secret.charAt(i)) {
+        bulls++;
         for (int j = 0; j < secret.length(); j++) {
-          if (guess.charAt(j) == secret.charAt(i) && i != j) {
+          if (userString.charAt(j) == secret.charAt(i) && i != j) {
             pigs++;
           }
         }
       }
-      turnCounter++;
-      if (bulls > 0 && pigs > 0) {
-        System.out.println("! Turn: " + turnCounter + "  Bull(s): " + bulls + " Pig(s): " + pigs);
-      } else if (bulls > 0 && pigs == 0 && bulls != secret.length()) {
-        System.out.println("@ Turn: " + turnCounter + "  Bull(s): " + bulls);
-      } else if (bulls == 0 && pigs > 0) {
-        System.out.println("# Turn: " + turnCounter + "  Pig(s): " + pigs);
-      } else if (bulls == secret.length()) {
-        do {
-          System.out.println("$ Turn: " + turnCounter + "  Bull(s): " + bulls);
-          System.out.println("Congratulations! You guessed the secret code.");
-          System.out.println("Would you like to play again?");
-          try {
-            guess = buffer.readLine().toUpperCase().trim();
-            if (guess.matches("^['Y']{1}$")) {
-              countBullsAndPigs();
-            } else {
-              return;
-            }
-          } catch (IOException nothing) {
-            //do nothing
-          }
-        } while (guess == "Y");
-      } else {
-        System.out.println("% Result: No matches.");
+    }*/
+    for (int i = 0; i < NUMBER_OF_CHARACTERS; i++) {
+      if (userString.charAt(i) == secret.charAt(i)) {
+        bulls++;
       }
+      else if (secret.contains(userString.charAt(i) + "")) {
+        pigs++;
+      }
+    }
+    turnCounter++;
+  }
+
+  private boolean getResult() {
+    if (turnCounter == 5) {
+      messages.displayBullsAndPigs(bulls, pigs);
+      messages.displayNoMoreAttemptsLeft();
+      messages.displaySecret(secret);
+      return true;
+    } else if (bulls == NUMBER_OF_CHARACTERS) {
+      messages.displayTheEnd();
+      return true;
+    } else {
+      messages.displayBullsAndPigs(bulls, pigs);
       bulls = 0;
       pigs = 0;
+      return false;
     }
   }
+
+  private boolean verifyOnlyCharInput() {
+    try {
+      messages.displayAttempts(turnCounter);
+      userString = buffer.readLine().toUpperCase().trim();
+      checkUserInput(userString);
+      return true;
+    } catch (IllegalArgumentException | IOException e) {
+      System.err.println(e.getMessage());
+    }
+    return false;
+  }
+
+  private void checkUserInput(String userInput) {
+    validateUserInput.checkForCharacterInput(userInput);
+    validateUserInput.checkOversizedWord(userInput);
+    validateUserInput.checkUndersizedWord(userInput);
+  }
+
+  void setSecret() {
+    //secret = "WHILE";
+    //create an instance of the WordList class, and then invoking a method on it.
+    //WordList wordList = new WordList();
+    //secret = wordList.generateRandomWord().toString();
+  }
+
+  void setUserString(String userString){
+    this.userString = userString;
+  }
+
+  int getBulls() {
+    return bulls;
+  }
+
+  int getPigs() {
+    return pigs;
+  }
+
+  String getSecret() {
+    return secret;
+  }
+
+  /*  public static void playAgain() {
+    Reader reader = new InputStreamReader(System.in);
+    BufferedReader buffer = new BufferedReader(reader);
+    String userInput = null;
+    do {
+      System.out.println("Would you like to play again?");
+      try {
+        userInput = buffer.readLine().toUpperCase().trim();
+        if (userInput.matches("^['Y']{1}$")) {
+          turnCounter = 0;
+          main();
+        } else {
+          turnCounter = 0;
+        }
+      } catch (IOException nothing) {
+        //do nothing
+      }
+    } while (userInput == "Y");
+  }*/
+
 }
