5be6307f9bdd125dd91e782898fa81485485ea67
craigkelleher <kelleher.craig@gmail.com>
2022-10-05 14:12:05 -0700

    Update user input validator

--- src/main/java/UserInputValidator.java
+++ src/main/java/UserInputValidator.java
@@ -1,30 +1,19 @@
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 class UserInputValidator {
 
-  void validateIfCharactersAreAllDifferent(String userInput) {
-    if (Arrays.stream(userInput.split(""))
-        .map(Integer::parseInt)
-        .collect(Collectors.toSet()).size() != PigsBulls.NUMBER_OF_CHARACTERS) {
-      throw new IllegalArgumentException("The characters have to be all different!");
-    }
-  }
 
-  void validateIfLengthIsLessThanFive(String userInput) {
+  void checkUndersizedWord(String userInput) {
     if (userInput.length() < PigsBulls.NUMBER_OF_CHARACTERS) {
       throw new IllegalArgumentException("You must provide at least 5 characters!");
     }
   }
 
-  void validateIfLengthIsMoreThanFive(String userInput) {
+  void checkOversizedWord(String userInput) {
     if (userInput.length() > PigsBulls.NUMBER_OF_CHARACTERS) {
       throw new IllegalArgumentException("You can provide only 5 characters!");
     }
   }
 
-  void validateIfOnlyCharacters(String userInput) {
+  void checkForCharacterInput(String userInput) {
     if (!userInput.matches("^[A-Z]{5}$") && userInput.length() == PigsBulls.NUMBER_OF_CHARACTERS) {
       throw new IllegalArgumentException("You can provide only characters!");
     }
